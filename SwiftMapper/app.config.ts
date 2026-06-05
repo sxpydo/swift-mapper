c989f8ae610acbfb29712ec6cef3f1d0110a05b0
sxpydo <shavypeynado@gmail.com>
2026-06-05 15:37:37 +0100

    swiftmapper(fix): move Google Maps API key to environment variable

---
+++ SwiftMapper/app.config.ts
@@ -0,0 +1,34 @@
+export default {
+  expo: {
+    name: "SwiftMapper",
+    slug: "SwiftMapper",
+    version: "1.0.0",
+    scheme: "swiftmapper",
+    main: "expo-router/entry",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/android-icon-foreground.png",
+        backgroundImage: "./assets/android-icon-background.png",
+        monochromeImage: "./assets/android-icon-monochrome.png",
+      },
+      predictiveBackGestureEnabled: false,
+      package: "com.shavs.swiftmapper",
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+        },
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: ["expo-router", "expo-status-bar"],
+  },
+};
